21c0f2e620f7960beb3790ec15d34534461ec0a6
Zerol Acqua <hlc118@outlook.com>
2023-05-12 00:24:25 +0800

    add-two-numbers

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -20,6 +20,35 @@ class Solution
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
     {
+        // 在两条链表上的指针
+        ListNode *p1 = l1, *p2 = l2;
+        // 虚拟头结点（构建新链表时的常用技巧）
+        ListNode *dummy = new ListNode(-1);
+        // 指针 p 负责构建新链表
+        ListNode *p = dummy;
+        // 记录进位
+        int carry = 0;
+        // 开始执行加法，两条链表走完且没有进位时才能结束循环
+        while (p1 != nullptr || p2 != nullptr || carry > 0) {
+            // 先加上上次的进位
+            int val = carry;
+            if (p1 != nullptr) {
+                val += p1->val;
+                p1 = p1->next;
+            }
+            if (p2 != nullptr) {
+                val += p2->val;
+                p2 = p2->next;
+            }
+            // 处理进位情况
+            carry = val / 10;
+            val = val % 10;
+            // 构建新节点
+            p->next = new ListNode(val);
+            p = p->next;
+        }
+        // 返回结果链表的头结点（去除虚拟头结点）
+        return dummy->next;
     }
 };
 // @lc code=end
